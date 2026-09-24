cb0f4f3ea9b67af7cc1140c6f482b74640c4e0ba
Nico Roig <nicolasroig88@hotmail.com>
2026-09-24 19:54:36 -0300

    Primera entrega de JS

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Variables:*/
+
+let nombre = prompt("Ingrese su nombre completo");
+
+let año = prompt("Ingrese el año en el que nació con números");
+
+let añoActual = 2026;
+
+let edad = añoActual - parseInt(año);
+
+alert(nombre + " Tenes la edad de: " + edad);
+
+
